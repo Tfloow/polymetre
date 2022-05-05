8c0210d664b1f0fddb5dbb4f668d8e3cc3fc255b
Tfloow <88899420+Tfloow@users.noreply.github.com>
2022-05-05 18:06:38 +0200

    Update demos_zone_stab.py

--- demos_zone_stab.py
+++ demos_zone_stab.py
@@ -1,76 +1,155 @@
-#%%
-import numpy as np
-from matplotlib import pyplot as plt
-pi = np.pi
+import time
+from dht11 import DHT11
+import machine
 
-x,y = np.meshgrid(np.linspace(-3,3,1000),np.linspace(-3,3,1000))
-z = x + 1j*y
+IR = machine.Pin(2, machine.Pin.IN, machine.Pin.PULL_UP)
+GAS = machine.Pin(6, machine.Pin.IN, machine.Pin.PULL_UP)
+led = machine.Pin(15, machine.Pin.OUT)
+led = machine.PWM(led)
+servo = machine.PWM(machine.Pin(7))
+sound = machine.ADC(27)
 
-# La méthode de Heun
-gain = np.abs(1+ z + z*z/2)
+servo.freq(50)
+servo.duty_u16(1638)
+led.duty_u16(0)
+timing = 60
 
-ax = plt.gca()
-ax.axis('equal')
-ax.axhline(y=0,color='k')
-ax.axvline(x=0,color='k')
-ax.yaxis.grid(color='gray',linestyle='dashed')
-ax.xaxis.grid(color='gray',linestyle='dashed')
-plt.title("Zone de stabilité de la méthode de Heun")
-plt.xlim((-2.5,2.5))
-plt.ylim((-2.5,2.5))
-plt.xlabel(r"Re($h \lambda$)")
-plt.ylabel(r"Im($h \lambda$)")
-plt.contour(x,y,gain,np.linspace(0, 1, 11) ,colors='black', linewidths=0.5)
-plt.contourf(x,y,gain,np.linspace(0, 1, 11) ,cmap='jet_r')
-plt.colorbar()
-plt.show()
+def servoSecure(pourcentage):
+    valueServo = [1638, 8192]
+    return int(valueServo[0]+pourcentage*valueServo[-1])
+    
 
 
+def readTaHData():
+    DATA = dht.read_data()    
+    t = DATA[0]
+    h = DATA[1]                                                    
+    return [str(t),str(h)]
 
+def read_Data(self):
+    global dat
+    global code_dis_flag
+    if IR.value() == 1:
+        lol = 0
+    else:
+        cnt = 0
+        while IR.value()==0 and cnt < 400:
+            time.sleep_us(7)
+            cnt +=1
+            continue
+        if cnt >= 400 or cnt < 230:
+            lol = 0
+        else:
+            if IR.value()==1:
+                cnt = 0
+                while IR.value()==1 and cnt < 215:
+                    time.sleep_us(7)
+                    cnt +=1
+                    continue
+                if cnt >= 215 or cnt < 100:
+                    lol = 0
+                else:
+                    data=[]
+                    j=0
+                    while j<32:
+                        cnt = 0    
+                        while IR.value()==0 and cnt < 30:
+                            time.sleep_us(7)
+                            cnt +=1
+                            continue
+                        if cnt >= 30 or cnt < 10:
+                            print('CL_bit',j)
+                        else:
+                            cnt = 0 
+                            while IR.value()==1 and cnt < 70:
+                                time.sleep_us(7)
+                                cnt +=1
+                                continue
+                            if cnt >= 70 or cnt < 10:
+                                print('CH_bit',j)
+                            else:
+                                if cnt > 35:
+                                    data.append(1)
+                                else:
+                                    data.append(0)
+                        j+=1            
+                    
+                    code3 = data[16:24]
+                    code4 = data[24:32]
+                    code3_buf = 0
+                    code4_buf = 0
+                    for i in range(8):
+                        code3_buf+=code3[i]*2**(7-i)
+                        code4_buf+=code4[i]*2**(7-i)
+                    if code3_buf+code4_buf == 255:
+                        dat = code3_buf
+                        print('IR code:',dat)                       
+                        code_dis_flag = 1
 
+IR.irq(handler=read_Data, trigger=machine.Pin.IRQ_FALLING)                     #IO Interrupts
+dat = 0
+code_dis_flag = 0
+old = 0
 
-# La méthode d'Adam-Basforth-Moulton d'ordre 2
-b = 1+z+3/4*z**2
-c = z**2 / 4
-s1 = 1/2*(b-np.sqrt(b**2 - 4*c))
-s2 = 1/2*(b+np.sqrt(b**2 - 4*c))
-gain = np.maximum(np.abs(s1), np.abs(s2))
-
-ax = plt.gca()
-ax.axis('equal')
-ax.axhline(y=0,color='k')
-ax.axvline(x=0,color='k')
-ax.yaxis.grid(color='gray',linestyle='dashed')
-ax.xaxis.grid(color='gray',linestyle='dashed')
-plt.title("Zone de stabilité de la méthode\n d'Adam-Basforth-Mouton d'ordre 2")
-plt.xlim((-2.5,2.5))
-plt.ylim((-2.5,2.5))
-plt.xlabel(r"Re($h \lambda$)")
-plt.ylabel(r"Im($h \lambda$)")
-plt.contour(x,y,gain,np.linspace(0, 1, 11) ,colors='black', linewidths=0.5)
-plt.contourf(x,y,gain,np.linspace(0, 1, 11) ,cmap='jet_r')
-plt.colorbar()
-plt.show()
-
-
-
-
-
-# la méthode de Cranck-Nicolson
-gain = np.abs((1+z/2)/(1-z/2))
-
-ax = plt.gca()
-ax.axis('equal')
-ax.axhline(y=0,color='k')
-ax.axvline(x=0,color='k')
-ax.yaxis.grid(color='gray',linestyle='dashed')
-ax.xaxis.grid(color='gray',linestyle='dashed')
-plt.title("Zone de stabilité de la méthode de Cranck-Nicolson")
-plt.xlim((-2.5,2.5))
-plt.ylim((-2.5,2.5))
-plt.xlabel(r"Re($h \lambda$)")
-plt.ylabel(r"Im($h \lambda$)")
-plt.contour(x,y,gain,np.linspace(0, 1, 11) ,colors='black', linewidths=0.5)
-plt.contourf(x,y,gain,np.linspace(0, 1, 11) ,cmap='jet_r')
-plt.colorbar()
-plt.show()
+while True:    
+    if dat == 162:
+        old = 162
+        dht = DHT11(3) 
+        res = readTaHData()
+        temp = int(res[0])
+        if temp > 40:
+            break
+        led.duty_u16(int(65534*temp/40))
+        servo.duty_u16(servoSecure(temp/40))
+        print(temp, type(temp))
+        time.sleep(0.5)
+    if dat == 98:
+        old = 98
+        dht = DHT11(3) 
+        res = readTaHData()
+        hum = int(res[1])
+        led.duty_u16(int(65534*hum/100))
+        servo.duty_u16(servoSecure(hum/100))
+        time.sleep(0.5)
+        print(hum)
+    if dat == 176:
+        old = 176
+        time.sleep(30/timing)
+        led.duty_u16(65534)
+        servo.duty_u16(1638)
+        time.sleep(30/timing)
+        led.duty_u16(0)
+        servo.duty_u16(3276)
+    if dat == 24:
+        timing += 10
+        print("incremented")
+        dat = old
+    if dat == 74:
+        if timing >10:
+            timing-= 10
+            print("lowered")
+        else:
+            print("could not go lower")
+        dat = old
+    if dat == 226:
+        old = 226
+        if GAS.value():
+            led.duty_u16(0)
+            servo.duty_u16(1638)
+            print("no problem")
+            time.sleep(1)
+        else:
+            led.duty_u16(3000)
+            servo.duty_u16(8192)
+            time.sleep(1)
+    if dat == 34:
+        old = 34
+        print(sound.read_u16())
+        led.duty_u16(sound.read_u16())
+        servo.duty_u16(servoSecure(sound.read_u16()/65534))
+        time.sleep(0.1)
+    if dat == 104:
+        print("Program ended")
+        led.duty_u16(0)
+        servo.duty_u16(1638)
+        break
